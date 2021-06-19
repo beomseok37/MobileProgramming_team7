8918771c7c5fb4aae11eb6fa5952452d478fc8e8
yjh0816 <2543kt@gmail.com>
2021-06-19 20:42:00 +0900

    이메일 인증 업데이트

--- app/src/main/java/com/example/honbap/SignUpActivity.kt
+++ app/src/main/java/com/example/honbap/SignUpActivity.kt
@@ -8,18 +8,24 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.example.honbap.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import java.util.*
+import kotlin.collections.HashMap
+import kotlinx.coroutines.*
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var binding2: ActivitySignUpBinding
     lateinit var rdb: DatabaseReference
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding2 = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding2.root)
+        auth = FirebaseAuth.getInstance()
         init2()
     }
-
     fun builddialog(text:String,context: Context){
         val builder = AlertDialog.Builder(context)
         builder.setMessage("$text").setTitle("$text")
@@ -60,9 +66,11 @@ class SignUpActivity : AppCompatActivity() {
             }
             emailcertificatebtn.setOnClickListener {
                 //이메일 인증
-                val checkemail=emailtextinputedittext.text.toString()
-                emailindatabase(auto,checkemail)
 
+                val checkemail=emailtextinputedittext.text.toString()
+//                val userpassword = passwordedittext.text.toString()
+                emailindatabase(auto,checkemail)
+                createUserId(checkemail,"password")
                 certificateflag=true
             }
             sexradiogroup.setOnCheckedChangeListener { _, checkedId ->
@@ -105,27 +113,70 @@ class SignUpActivity : AppCompatActivity() {
                     Log.i("join","nickname")
                 }
                 else {
+                    auth.currentUser?.reload()
+
                     Log.i("join","else")
                     useremail = emailtextinputedittext.text.toString()
                     userpassword = passwordedittext.text.toString()
                     userage = ageedittext.text.toString().toInt()
                     usernickname = nickname.text.toString()
                     //firebase에 넘겨야겠지..
+                    if(verifyEmail2()) {
+                        val loginrdb = rdb.child("$auto")
+                        loginrdb.child("uid").setValue(auto)
+                        loginrdb.child("userEmail").setValue(useremail)
+                        loginrdb.child("userPassword").setValue(userpassword)
+                        loginrdb.child("userage").setValue(userage)
+                        loginrdb.child("userSex").setValue(usersex)
+                        loginrdb.child("userNickname").setValue(usernickname)
+                        val hashmap: HashMap<String, Any> = HashMap<String, Any>()
+                        hashmap.put("auto", "${auto + 1}")
+                        val addauto = rdb.updateChildren(hashmap)
+                        finish()
+                    } else {
+                        Toast.makeText(this@SignUpActivity,"이메일 인증을 해주세요",Toast.LENGTH_SHORT).show()
+                    }
 
-                    val loginrdb = rdb.child("$auto")
-                    loginrdb.child("uid").setValue(auto)
-                    loginrdb.child("userEmail").setValue(useremail)
-                    loginrdb.child("userPassword").setValue(userpassword)
-                    loginrdb.child("userage").setValue(userage)
-                    loginrdb.child("userSex").setValue(usersex)
-                    loginrdb.child("userNickname").setValue(usernickname)
-                    val hashmap: HashMap<String, Any> = HashMap<String, Any>()
-                    hashmap.put("auto", "${auto + 1}")
-                    val addauto = rdb.updateChildren(hashmap)
-                    finish()
                 }
             }
         }
+    }
+
+    fun createUserId(id:String, pw:String) {
+        Log.v("1", "1")
+        auth.createUserWithEmailAndPassword(id,pw).addOnCompleteListener { task->
+            if(task.isSuccessful) {
+                Log.v("success", "success")
+                verifyEmail()
+            } else {
+                Log.v("asd","Asd")
+                Log.w("createUserWithEmail:failure", task.exception)
+//                Toast.makeText(this@SignUpActivity,"회갑실패",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    fun verifyEmail() {
+        auth.currentUser?.sendEmailVerification()?.addOnCompleteListener {
+            if(it.isSuccessful) {
+                Log.v("emailsend2", "Email sent.2")
+                Toast.makeText(this@SignUpActivity,"이메일이 전송 되었습니다.",Toast.LENGTH_SHORT).show()
+            } else {
+                Log.v("emailsend2", "Email sent failed.2")
+                Toast.makeText(this@SignUpActivity,"이메일 전송이 실패했습니다.",Toast.LENGTH_SHORT).show()
+                auth.currentUser!!.delete()
+            }
+        }
+    }
+    fun verifyEmail2():Boolean {
+        runBlocking {
+            val job = GlobalScope.launch {
+                auth.currentUser?.reload()
+            }
+            job.join()
+        }
+        Toast.makeText(this@SignUpActivity,"이메일 인증 확인중입니다.",Toast.LENGTH_SHORT).show()
+        Thread.sleep(2000L)
+        return auth.currentUser!!.isEmailVerified
     }
     fun emailindatabase(count:Int,userid:String){
         var flag=false
@@ -136,7 +187,7 @@ class SignUpActivity : AppCompatActivity() {
             override fun onDataChange(snapshot: DataSnapshot) {
                 for(i in 0..(count-1)){
                     val tempemail=snapshot.child("$i").child("userEmail").value.toString()
-                    Log.i("email",tempemail)
+//                    Log.i("email",tempemail)
                     if(userid==tempemail){
                         flag=true
                         break
@@ -161,3 +212,4 @@ class SignUpActivity : AppCompatActivity() {
         dlg.show()
     }
 }
+
